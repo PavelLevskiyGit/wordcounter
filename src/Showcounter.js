b6935051c6a6da4483b217ce33b8eb48480149c7
PavelLevskiyGit <pavellevskiyua@gmail.com>
2021-07-22 19:10:00 +0300

    add showcounter element

---
+++ src/Showcounter.js
@@ -0,0 +1,12 @@
+import React from "reacr";
+import ReactDOM from "react-dom";
+import "./showcounter.css";
+
+const fs = require("fs");
+
+function ShowCounter () {
+  return(
+    <div>
+    </div>
+  );
+}
