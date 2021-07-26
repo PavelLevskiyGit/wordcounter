bf731c80cb02fee4fa9af8d61c32574e64e452d3
PavelLevskiyGit <pavellevskiyua@gmail.com>
2021-07-26 11:53:59 +0300

    edit input file

--- src/Input.js
+++ src/Input.js
@@ -5,9 +5,14 @@ import writeBase from "./writebase.js";
 
 function Input(){
   const [data, setData] = useState([{},{},{}]);
-  const conf = data => window.confirm(`all good? ${data[0] == undefined ? "word is empty" : data[0]} 
-    ${data[1] == undefined ? "translation is empty" : data[1]} ${data[2] == undefined ? "transcription is empty" : data[2]}`);
+  
+  const conf = data => window.confirm(`all good? 
+    ${data[0].word == undefined ? "word is empty" : data[0].word} 
+    ${data[1].translation == undefined ? "translation is empty" : data[1].translation} 
+    ${data[2].transcription == undefined ? "transcription is empty" : data[2].transcription}`);
+    
   console.log("data", data);
+  
   return(
     <div className = "inputDiv">
       <input onChange = {e => setData([{"word" : e.target.value},{},{}])} placeholder = "write some words"/>
