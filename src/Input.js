6e94a52e88150e9d24735505091756af79395090
PavelLevskiyGit <pavellevskiyua@gmail.com>
2021-07-26 12:34:07 +0300

    edit input file

--- src/Input.js
+++ src/Input.js
@@ -8,12 +8,12 @@ function Input(){
   
   const conf = data => window.confirm(`all good? 
     ${[...data][0].word == undefined ? "word is empty" : [...data][0].word} 
-    ${[...data][1].translation == undefined ? "translation is empty" : [...data][1].translation} 
+    ${[...data][1].translate == undefined ? "translation is empty" : [...data][1].translation} 
     ${[...data][2].transcription == undefined ? "transcription is empty" : [...data][2].transcription}`);
     
   console.log("data", data);
   console.log("data 0", data[0].word);
-  console.log("data 1", data[1].translation);
+  console.log("data 1", data[1].translate);
   console.log("data 2", data[2].transcription);
   
   return(
