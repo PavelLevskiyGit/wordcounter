a2a8c0ddada0595e79fe764ae4b85bdab334f7cc
PavelLevskiyGit <pavellevskiyua@gmail.com>
2021-07-26 10:28:10 +0300

    create imput file

--- src/Input.js
+++ src/Input.js
@@ -4,13 +4,16 @@ import "./Input.css";
 import writeBase from "./writebase.js";
 
 function Input(){
-  const [data, setData] = useState('');
+  const [data, setData] = useState([]);
+  const confirm = data => confirm("all good", data[0], data[1], data[2])
   return(
     <div className = "inputDiv">
-      <input onChange = {e => setData(e.target.value)} placeholder = "write some words"/>
-      <input placeholder = "write transcription"/>
-      <input placeholder = "write translate"/>
-      <button onClick = { data => writeBase(data) } >add</button>
+      <input onChange = {e => setData([...data][0] = e.target.value)} placeholder = "write some words"/>
+      <input onChange = {e => setData([...data][1] = e.target.value)} placeholder = "write transcription"/>
+      <input onChange = {e => setData([...data][2] = e.target.value)} placeholder = "write translate"/>
+      <button onClick = { data => {
+        confirm(data) ? writeBase(data) && setState([]) : alert("correct please") } 
+        } >add</button>
     </div>
   );
 };
