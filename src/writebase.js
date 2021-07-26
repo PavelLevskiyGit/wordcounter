d2da84dcc601b58adc30f86cb9bf0929bd61b5d3
PavelLevskiyGit <pavellevskiyua@gmail.com>
2021-07-26 13:27:51 +0300

    edit input file

--- src/writebase.js
+++ src/writebase.js
@@ -6,6 +6,6 @@ const writeData = data => [...base, data];
 fs.writeFile('./base.json', writeData(), 'utf-8', err => {
   if (err) throw err;
   console.log('all done');
-});*/
+});
 
 export default writeData;
